e472f71a57593203f4584cace8bc0d5ce78b8a56
DESKTOP-D7NMFO9\Sandeep <www.sandeepbabu45gmail.com>
2019-08-22 02:28:41 +0530

    generate 256bit ecdsa key-pair

--- app/src/main/java/com/sandeep/hashcalculator/MainActivity.kt
+++ app/src/main/java/com/sandeep/hashcalculator/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.sandeep.hashcalculator
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -16,7 +17,6 @@ import java.security.Signature
 class MainActivity : AppCompatActivity() {
 
     var input: String? = null
-    var ecdInput: String? = null
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,8 +30,9 @@ class MainActivity : AppCompatActivity() {
                 showHash.text = "Calculated Hash"
             }
         }
-        calcEC.setOnClickListener {
-            generateECDSAKey()
+        nextSig.setOnClickListener {
+            val intent = Intent(this@MainActivity, EcKeyGen::class.java)
+            startActivity(intent)
         }
 
     }
@@ -62,41 +63,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    @SuppressLint("SetTextI18n")
-    private fun generateECDSAKey(){
-        val keyGen = KeyPairGenerator.getInstance("EC")
-        val random = SecureRandom.getInstance("SHA1PRNG")
 
-        keyGen.initialize(256, random)
-        val pair = keyGen.generateKeyPair()
-        val priv = pair.private
-        val pub = pair.public
-      //  Log.d("keys", "pub: ${pub.hashCode()} \n priv: ${priv.hashCode()}")
-
-
-        val ecd = Signature.getInstance("SHA256withECDSA")
-        ecd.initSign(priv)
-
-        ecdInput = enterDataEC.text.toString()
-        Log.d("input", "input:$ecdInput")
-        val ecdSha = hexString(stringtoByteArray(ecdInput!!))
-     //   Log.d("input", "ecdsha:$ecdSha")
-
-
-        val strByte = ecdInput?.toByteArray(StandardCharsets.UTF_8)
-        ecd.update(strByte)
-        val realSig = ecd.sign()
-        val sig = "Signature: ${BigInteger(1, realSig).toString(16)}"
-      //  Log.d("sig", "Sign:$sig \n len:${sig.length}")
-
-
-        if (ecdInput?.length!! > 0) {
-            dispECD.text = "Public Key: $pub \n Private Key: $ecdSha"
-            disSig.text = sig
-        }else{
-            dispECD.text = "Public Key to show"
-            disSig.text = "Signature to show"
-        }
-
-    }
 }
